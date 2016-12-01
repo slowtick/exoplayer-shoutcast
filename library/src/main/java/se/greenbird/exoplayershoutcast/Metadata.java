b3ed6fc3d716bb78001c33eb5bf98e66df62b0c5
Mattias Karlsson <mattias.erik.karlsson@gmail.com>
2016-12-01 08:43:38 +0100

    Revert "Simplified Metadata class to only include info about current song/artist/show"
    This reverts commit ec02761b08c0827dd4ef94eaf3b712fbeb5fc7a9.

--- library/src/main/java/se/greenbird/exoplayershoutcast/Metadata.java
+++ library/src/main/java/se/greenbird/exoplayershoutcast/Metadata.java
@@ -20,11 +20,21 @@ public class Metadata {
     private final String artist;
     private final String song;
     private final String show;
+    private final String channels;
+    private final String bitrate;
+    private final String station;
+    private final String genre;
+    private final String url;
 
-    public Metadata(String artist, String song, String show) {
+    public Metadata(String artist, String song, String show, String channels, String bitrate, String station, String genre, String url) {
         this.artist = artist;
         this.song = song;
         this.show = show;
+        this.channels = channels;
+        this.bitrate = bitrate;
+        this.station = station;
+        this.genre = genre;
+        this.url = url;
     }
 
     public String getArtist() {
@@ -37,5 +47,25 @@ public class Metadata {
 
     public String getShow() {
         return show;
+    }
+
+    public String getChannels() {
+        return channels;
+    }
+
+    public String getBitrate() {
+        return bitrate;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
